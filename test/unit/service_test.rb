d28d8ece056941f5938c2f5b1f789f7438f8574a
Hallelujah <hery@rails-royce.org>
2018-12-25 18:13:27 +0100

    [fix] FactoryGirl => FactoryBot

--- test/unit/service_test.rb
+++ test/unit/service_test.rb
@@ -138,7 +138,7 @@ class ServiceTest < ActiveSupport::TestCase
   end
 
   test 'service_token' do
-    service = Service.new { |s| s.account = FactoryGirl.create(:simple_account); s.system_name = 'foo' }
+    service = Service.new { |s| s.account = FactoryBot.create(:simple_account); s.system_name = 'foo' }
     service.save(validate: false)
 
     refute service.service_token
