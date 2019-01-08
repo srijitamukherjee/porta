59e575a5a533f0b7d70ab255eacc0e6bfff9ceaf
Guilherme Cassolato <guicassolato@gmail.com>
2019-01-08 12:06:02 +0100

    Fix ProxyConfig#clone_to for postgres

--- app/models/proxy_config.rb
+++ app/models/proxy_config.rb
@@ -39,7 +39,7 @@ class ProxyConfig < ApplicationRecord
       .when_having { max(table.version) == version }
       .group(:id)
 
-    System::Database.oracle? ? where(id: scope.group(:version).select(:id)) : scope
+    System::Database.mysql? ? scope : where(id: scope.group(:version).select(:id))
   end
 
   def differs_from?(comparable)
@@ -91,7 +91,7 @@ class ProxyConfig < ApplicationRecord
     config.update_all("version = 1 + (#{Arel.sql max_version.to_sql})")
 
     # Read the value
-    version = config.connection.select_value(config.select(:version))
+    version = config.connection.select_value(config.select(:version))&.to_i
     raw_write_attribute :version, version
   end
 
