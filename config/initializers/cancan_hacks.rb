64661e8f1479bf5e1819315c64bd5ffdfa79738e
Marta Noya <mnoyabon@redhat.com>
2019-09-02 13:07:33 +0200

    Fix NoMethodError: undefined method `actions' for nil:NilClass possible_relevant_rules does not return nil values

--- config/initializers/cancan_hacks.rb
+++ config/initializers/cancan_hacks.rb
@@ -1,1 +1,21 @@
+# frozen_string_literal: true
+
 require 'cancan_hacks'
+require 'cancan/ability/rules'
+
+module CanCan
+  module Ability
+    Rules.module_eval do
+      # possible_relevant_rules does not return `nil` values anymore
+      def possible_relevant_rules(subject)
+        if subject.is_a?(Hash)
+          rules
+        else
+          positions = @rules_index.values_at(subject, *alternative_subjects(subject))
+          positions.flatten!.sort!
+          positions.map { |i| @rules[i] }.compact
+        end
+      end
+    end
+  end
+end
